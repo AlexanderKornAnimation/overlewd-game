4cdb7fd4517317757634e1df38cb00153a11c03b
OrigamiShiro <76973214+OrigamiShiro@users.noreply.github.com>
2021-10-18 12:43:56 +0300

    fixed item and description size; changed grid layout to vertical layout (#24)

--- Assets/Scripts/UI/DebugScreens/DebugScreenViewer.cs
+++ Assets/Scripts/UI/DebugScreens/DebugScreenViewer.cs
@@ -10,7 +10,7 @@ namespace Overlewd
         
         void Start()
         {
-            string prefabPath = "Prefabs/UI/Screens/EventMapScreen/NewEventMap";
+            string prefabPath = "Prefabs/UI/Overlays/EventOverlay/EventOverlay";
             //prefabPath = "Prefabs/UI/DebugScreens/DebugContentViewer/ContentViewer";
             var screenPrefab = (GameObject)Instantiate(Resources.Load(prefabPath));
             var screenRectTransform = screenPrefab.GetComponent<RectTransform>();
