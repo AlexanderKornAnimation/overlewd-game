91943bd47bdd0d2830fb59e8fb54344240433162
kbb1025doge <vladimir.a@supadoge.com>
2021-11-21 19:53:14 +0300

    prepare Harem screen widget; implement draft logic

--- Assets/Scripts/UI/Screens/HaremScreen/HaremScreen.cs
+++ Assets/Scripts/UI/Screens/HaremScreen/HaremScreen.cs
@@ -7,6 +7,37 @@ namespace Overlewd
 {
     public class HaremScreen : BaseScreen
     {
+        private Button backButton;
+
+        private Button ulviButton;
+        private Image ulviGirl;
+        private Image ulviBuff;
+        private Text ulviDescription;
+        private Text ulviName;
+
+        private Button adrielButton;
+        private Image adrielGirl;
+        private Image adrielBuff;
+        private Text adrielDescription;
+        private Text adrielName;
+        private Transform adrielNotActive;
+
+        private Button ingieButton;
+        private Text ingieName;
+        private Transform ingieNotActive;
+
+        private Button fayeButton;
+        private Text fayeName;
+        private Transform fayeNotActive;
+
+        private Button liliButton;
+        private Text liliName;
+        private Transform liliNotActive;
+
+        private Button battleGirlsButton;
+        private Image battleGirlsGirl;
+        private Text battleGirlsTitle;
+
         void Start()
         {
             var screenPrefab = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Screens/HaremScreen/Harem"));
@@ -14,23 +45,85 @@ namespace Overlewd
             screenRectTransform.SetParent(transform, false);
             UIManager.SetStretch(screenRectTransform);
 
-            screenRectTransform.Find("Canvas").Find("Castle").GetComponent<Button>().onClick.AddListener(() =>
-            {
-                UIManager.ShowScreen<CastleScreen>();
-            });
+            var canvas = screenRectTransform.Find("Canvas");
 
-            screenRectTransform.Find("Canvas").Find("Girl1").GetComponent<Button>().onClick.AddListener(() =>
-            {
-                UIManager.ShowScreen<GirlScreen>();
-            });
+            backButton = canvas.Find("BackButton").GetComponent<Button>();
+            backButton.onClick.AddListener(BackButtonClick);
 
-            screenRectTransform.Find("Canvas").Find("Girl2").GetComponent<Button>().onClick.AddListener(() =>
-            {
-                UIManager.ShowScreen<GirlScreen>();
-            });
+            ulviButton = canvas.Find("UlviButton").GetComponent<Button>();
+            ulviButton.onClick.AddListener(UlviButtonClick);
+            ulviGirl = ulviButton.transform.Find("Girl").GetComponent<Image>();
+            ulviBuff = ulviButton.transform.Find("Buff").GetComponent<Image>();
+            ulviDescription = ulviButton.transform.Find("Description").GetComponent<Text>();
+            ulviName = ulviButton.transform.Find("Name").GetComponent<Text>();
+
+            adrielButton = canvas.Find("AdrielButton").GetComponent<Button>();
+            adrielButton.onClick.AddListener(AdrielButtonClick);
+            adrielGirl = adrielButton.transform.Find("Girl").GetComponent<Image>();
+            adrielBuff = adrielButton.transform.Find("Buff").GetComponent<Image>();
+            adrielDescription = adrielButton.transform.Find("Description").GetComponent<Text>();
+            adrielName = adrielButton.transform.Find("Name").GetComponent<Text>();
+            adrielNotActive = adrielButton.transform.Find("NotActive");
+
+            ingieButton = canvas.Find("IngieButton").GetComponent<Button>();
+            ingieButton.onClick.AddListener(IngieButtonClick);
+            ingieName = ingieButton.transform.Find("Name").GetComponent<Text>();
+            ingieNotActive = ingieButton.transform.Find("NotActive");
+
+            fayeButton = canvas.Find("FayeButton").GetComponent<Button>();
+            fayeButton.onClick.AddListener(FayeButtonClick);
+            fayeName = fayeButton.transform.Find("Name").GetComponent<Text>();
+            fayeNotActive = fayeButton.transform.Find("NotActive");
+
+            liliButton = canvas.Find("LiliButton").GetComponent<Button>();
+            liliButton.onClick.AddListener(LiliButtonClick);
+            liliName = liliButton.transform.Find("Name").GetComponent<Text>();
+            liliNotActive = liliButton.transform.Find("NotActive");
+
+            battleGirlsButton = canvas.Find("BattleGirlsButton").GetComponent<Button>();
+            battleGirlsButton.onClick.AddListener(BattleGirlsButtonClick);
+            battleGirlsGirl = battleGirlsButton.transform.Find("Girl").GetComponent<Image>();
+            battleGirlsTitle = battleGirlsButton.transform.Find("Title").GetComponent<Text>();
+
+            Customize();
         }
 
-        void Update()
+        private void Customize()
+        {
+
+        }
+
+        private void BackButtonClick()
+        {
+            UIManager.ShowScreen<CastleScreen>();
+        }
+
+        private void UlviButtonClick()
+        {
+            UIManager.ShowScreen<GirlScreen>();
+        }
+
+        private void AdrielButtonClick()
+        {
+            UIManager.ShowScreen<GirlScreen>();
+        }
+
+        private void IngieButtonClick()
+        {
+
+        }
+
+        private void FayeButtonClick()
+        {
+
+        }
+
+        private void LiliButtonClick()
+        {
+
+        }
+
+        private void BattleGirlsButtonClick()
         {
 
         }
