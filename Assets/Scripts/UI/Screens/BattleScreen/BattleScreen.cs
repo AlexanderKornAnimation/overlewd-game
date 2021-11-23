308712b8b8dba802b09d436ee95e1f2d6f10f5fd
kbb1025doge <vladimir.a@supadoge.com>
2021-11-23 16:16:36 +0300

    add test battle video to battle screens

--- Assets/Scripts/UI/Screens/BattleScreen/BattleScreen.cs
+++ Assets/Scripts/UI/Screens/BattleScreen/BattleScreen.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 namespace Overlewd
 {
@@ -9,6 +10,8 @@ namespace Overlewd
     {
         private Button startBattleButton;
         private Button backButton;
+
+        private VideoPlayer battleVideo;
 
         async void Start()
         {
@@ -25,14 +28,28 @@ namespace Overlewd
             backButton = canvas.Find("BackButton").GetComponent<Button>();
             backButton.onClick.AddListener(BackButtonClick);
 
+            battleVideo = canvas.Find("TestVideo").GetComponent<VideoPlayer>();
+
             await GameData.EventStageStartAsync(GameGlobalStates.battle_EventStageData);
         }
 
-        private async void StartBattleButtonClick()
+        private async void EndBattleVideo(VideoPlayer vp)
         {
+            backButton.gameObject.SetActive(true);
+            startBattleButton.gameObject.SetActive(true);
+
             await GameData.EventStageEndAsync(GameGlobalStates.battle_EventStageData);
 
             UIManager.ShowPopup<VictoryPopup>();
+        }
+
+        private void StartBattleButtonClick()
+        {
+            battleVideo.Play();
+            battleVideo.loopPointReached += EndBattleVideo;
+
+            backButton.gameObject.SetActive(false);
+            startBattleButton.gameObject.SetActive(false);
         }
 
         private void BackButtonClick()
