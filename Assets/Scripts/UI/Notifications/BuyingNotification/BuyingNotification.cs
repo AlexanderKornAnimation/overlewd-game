06ac7fbaa8100d706f0f890245084ccb369477fc
kbb1025doge <vladimir.a@supadoge.com>
2021-12-16 15:21:19 +0300

    BuyingNotification inherited from DialogNotification

--- Assets/Scripts/UI/Notifications/BuyingNotification/BuyingNotification.cs
+++ Assets/Scripts/UI/Notifications/BuyingNotification/BuyingNotification.cs
@@ -1,36 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Overlewd
 {
-    public class BuyingNotification : BaseNotification
+    public class BuyingNotification : DialogNotification
     {
-        protected Button button;
-        protected Image girlEmotion;
-        protected TextMeshProUGUI text;
 
-        protected virtual void Awake()
-        {
-            var screenPrefab = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Notifications/BuyingNotification/BuyingNotification"));
-            var screenRectTransform = screenPrefab.GetComponent<RectTransform>();
-            screenRectTransform.SetParent(transform, false);
-            UIManager.SetStretch(screenRectTransform);
-
-            var canvas = screenRectTransform.Find("Canvas");
-            
-            button = canvas.Find("Button").GetComponent<Button>();
-            button.onClick.AddListener(ButtonClick);
-
-            text = canvas.Find("Banner").Find("Text").GetComponent<TextMeshProUGUI>();
-            girlEmotion = canvas.Find("Banner").Find("GirlEmotion").GetComponent<Image>();
-        }
-
-        protected virtual void ButtonClick()
-        {
-            UIManager.HideNotification();
-        }
     }
 }
--- Assets/Scripts/UI/Notifications/DialogNotification/DialogNotification.cs
+++ Assets/Scripts/UI/Notifications/DialogNotification/DialogNotification.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Overlewd
 {
-    public class DialogNotification : BuyingNotification
+    public class DialogNotification : BaseNotification
     {
-        protected override void Awake()
-        {
-            base.Awake();
+        protected Button button;
+        protected TextMeshProUGUI text;
+        protected Transform emotionBack;
+        protected Transform emotionPos;
 
-            button.gameObject.SetActive(false);
+        protected virtual void Awake()
+        {
+            var screenPrefab = (GameObject)Instantiate(Resources.Load("Prefabs/UI/Notifications/DialogNotification/DialogNotification"));
+            var screenRectTransform = screenPrefab.GetComponent<RectTransform>();
+            screenRectTransform.SetParent(transform, false);
+            UIManager.SetStretch(screenRectTransform);
+
+            var canvas = screenRectTransform.Find("Canvas");
+ 
+            button = canvas.Find("Button").GetComponent<Button>();
+            button.onClick.AddListener(ButtonClick);
+
+            var banner = canvas.Find("Banner");
+            text = banner.Find("Text").GetComponent<TextMeshProUGUI>();
+            emotionBack = banner.Find("EmotionBack");
+            emotionPos = emotionBack.Find("EmotionPos");
+        }
+
+        protected virtual void ButtonClick()
+        {
+            UIManager.HideNotification();
         }
     }
 }
