d74c6564887fbe73c5de5b23716d9cc175faa09b
kbb1025doge <vladimir.a@supadoge.com>
2022-01-11 00:38:07 +0300

    add SoundManager empty class

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Overlewd
+{
+    public static class SoundManager
+    {
+        
+    }
+
+}
