ca64fecd164f7c392123251aaa03adfd20503e65
kbb1025doge <vladimir.a@supadoge.com>
2022-01-30 21:18:36 +0300

    draft custom build assetbundles

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -19,4 +19,20 @@ public class CreateAssetBundles
 		CheckBundlesDirectory(bundlesPath);
 		BuildPipeline.BuildAssetBundles(bundlesPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
 	}
+
+    [MenuItem("Assets/Build Asset Bundles Using BuildMap")]
+    static void BuildAssetBundlesCustom()
+    {
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[2];
+
+        buildMap[0].assetBundleName = "enemybundle";
+        string[] enemyAssets = new string[2];
+        enemyAssets[0] = "Assets/Textures/char_enemy_alienShip.jpg";
+        enemyAssets[1] = "Assets/Textures/char_enemy_alienShip-damaged.jpg";
+        buildMap[0].assetNames = enemyAssets;
+
+        var bundlesPath = "Assets/AssetsBundles";
+        CheckBundlesDirectory(bundlesPath);
+        BuildPipeline.BuildAssetBundles(bundlesPath, buildMap, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
 }
