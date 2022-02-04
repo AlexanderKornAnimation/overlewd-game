fda694fcffbcddbc964d798f4afa1af20aaaa777
kbb1025doge <vladimir.a@supadoge.com>
2022-02-04 16:07:30 +0300

    refact NetworkHelper

--- Assets/Scripts/NetworkHelper.cs
+++ Assets/Scripts/NetworkHelper.cs
@@ -11,24 +11,6 @@ namespace Overlewd
 {
     public static class NetworkHelper
     {
-        private static async Task WaitRequestDoneAsync(UnityWebRequest request)
-        {
-            while (!request.isDone)
-            {
-                await Task.Delay(10);
-            }
-        }
-
-        private static bool RequestCheckError(UnityWebRequest request)
-        {
-            if (request.result != UnityWebRequest.Result.ProtocolError &&
-                request.result != UnityWebRequest.Result.ConnectionError)
-            {
-                return false;
-            }
-            return true;
-        }
-
         public static bool HasNetworkConection()
         {
             return Application.internetReachability != NetworkReachability.NotReachable;
@@ -51,7 +33,10 @@ namespace Overlewd
             }
             request.SendWebRequest();
 
-            await WaitRequestDoneAsync(request);
+            while (!request.isDone)
+            {
+                await Task.Delay(10);
+            }
 
             return request;
         }
@@ -65,57 +50,12 @@ namespace Overlewd
             }
             request.SendWebRequest();
 
-            await WaitRequestDoneAsync(request);
+            while (!request.isDone)
+            {
+                await Task.Delay(10);
+            }
 
             return request;
-        }
-
-        public static async Task<AudioClip> LoadAudioClipAsync(string url, AudioType audioType)
-        {
-            using (var request = UnityWebRequestMultimedia.GetAudioClip(url, audioType))
-            {
-                request.SendWebRequest();
-
-                await WaitRequestDoneAsync(request);
-
-                if (!RequestCheckError(request))
-                {
-                    return DownloadHandlerAudioClip.GetContent(request);
-                }
-                return null;
-            }
-        }
-
-        public static async Task<Texture2D> LoadTexture2DAsync(string url)
-        {
-            using (var request = UnityWebRequestTexture.GetTexture(url))
-            {
-                request.SendWebRequest();
-
-                await WaitRequestDoneAsync(request);
-
-                if (!RequestCheckError(request))
-                {
-                    return DownloadHandlerTexture.GetContent(request);
-                }
-                return null;
-            }
-        }
-
-        public static async Task<AssetBundle> LoadAssetBundleAsync(string url)
-        {
-            using (var request = UnityWebRequestAssetBundle.GetAssetBundle(url))
-            {
-                request.SendWebRequest();
-
-                await WaitRequestDoneAsync(request);
-
-                if (!RequestCheckError(request))
-                {
-                    return DownloadHandlerAssetBundle.GetContent(request);
-                }
-                return null;
-            }
         }
     }
 }
