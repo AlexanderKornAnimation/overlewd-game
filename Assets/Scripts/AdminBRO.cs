93b083cc31c0efac16ca5c75f6a13c98f12a7f72
kbb1025doge <vladimir.a@supadoge.com>
2022-08-08 20:35:31 +0300

    Update AdminBRO.cs
    fix Battle pass

--- Assets/Scripts/AdminBRO.cs
+++ Assets/Scripts/AdminBRO.cs
@@ -1048,8 +1048,8 @@ namespace Overlewd
             public class Level
             {
                 public int pointsThreshold;
-                public int defaultReward;
-                public int premiumReward;
+                public RewardItem defaultReward;
+                public RewardItem premiumReward;
             }
         }
 
