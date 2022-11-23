d0e5a8d2dea5acdf11a1801b85f9244a288d8693
kbb1025doge <vladimir.a@supadoge.com>
2022-11-23 13:04:58 +0300

    refact serer connection notif

--- Assets/Scripts/UI/Widgets/Notifications/System/ServerConnectionNotif.cs
+++ Assets/Scripts/UI/Widgets/Notifications/System/ServerConnectionNotif.cs
@@ -10,48 +10,42 @@ namespace Overlewd
 {
     public class ServerConnectionNotif : MonoBehaviour
     {
-        enum State
-        {
-            Show,
-            Hide
-        }
-
         private Transform back;
-        private CanvasGroup backCG;
+        private TextMeshProUGUI title;
         private Transform progress;
 
         private Coroutine hideTimerCoroutine;
-        private float backAlphaTime;
-        private float progressAngle;
-        private float backMasterAlpha;
-        private State state = State.Show;
+
+        private Sequence titleSeq;
+        private Sequence progressSeq;
 
         void Awake()
         {
-            var canvas = transform.Find("Canvas");
-            back = canvas.Find("Back");
-            backCG = back.GetComponent<CanvasGroup>();
+            back = transform.Find("Back");
+            title = back.Find("Title").GetComponent<TextMeshProUGUI>();
             progress = back.Find("Progress");
         }
 
         void Start()
         {
-            backCG.alpha = 0.6f;
-            backMasterAlpha = 1.0f;
-            StartCoroutine(AnimProgress());
-        }
+            titleSeq = DOTween.Sequence();
+            titleSeq.AppendCallback(() => title.text = "Connecting");
+            titleSeq.AppendInterval(0.4f);
+            titleSeq.AppendCallback(() => title.text = "Connecting.");
+            titleSeq.AppendInterval(0.4f);
+            titleSeq.AppendCallback(() => title.text = "Connecting..");
+            titleSeq.AppendInterval(0.4f);
+            titleSeq.AppendCallback(() => title.text = "Connecting...");
+            titleSeq.AppendInterval(0.4f);
+            titleSeq.SetLoops(-1);
+            titleSeq.Play();
 
-        private IEnumerator AnimProgress()
-        {
-            while (true)
-            {
-                backCG.alpha = backMasterAlpha;
-                progress.transform.localEulerAngles = new Vector3(0.0f, 0.0f, progressAngle);
-                yield return new WaitForSeconds(0.01f);
-                backAlphaTime += 0.01f;
-                progressAngle += 8.0f;
-                backMasterAlpha = Mathf.Clamp01(state == State.Show ? backMasterAlpha + 0.1f : backMasterAlpha - 0.04f);
-            }
+            progressSeq  = DOTween.Sequence();
+            progressSeq.Append(progress.transform.DOLocalRotateQuaternion(Quaternion.Euler(0.0f, 0.0f, 120.0f), 0.2f).SetEase(Ease.Linear));
+            progressSeq.Append(progress.transform.DOLocalRotateQuaternion(Quaternion.Euler(0.0f, 0.0f, 240.0f), 0.2f).SetEase(Ease.Linear));
+            progressSeq.Append(progress.transform.DOLocalRotateQuaternion(Quaternion.Euler(0.0f, 0.0f, 360.0f), 0.2f).SetEase(Ease.Linear));
+            progressSeq.SetLoops(-1);
+            progressSeq.Play();
         }
 
         public void Show()
@@ -61,8 +55,6 @@ namespace Overlewd
                 StopCoroutine(hideTimerCoroutine);
                 hideTimerCoroutine = null;
             }
-            state = State.Show;
-            backMasterAlpha = 1.0f;
         }
 
         public void Hide()
@@ -75,9 +67,9 @@ namespace Overlewd
 
         private IEnumerator HideTimer()
         {
-            yield return new WaitForSeconds(2.5f);
-            state = State.Hide;
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.8f);
+            titleSeq.Kill();
+            progressSeq.Kill();
             Destroy(gameObject);
         }
 
